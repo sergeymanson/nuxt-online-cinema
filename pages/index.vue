d23594f3f6c1e8ae0adefd54992d874da42765a7
dartchuk <manson1989>
2018-06-04 23:21:20 +0300

    start dev

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="container">
+qqq
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'index-page',
+  layout: 'default',
+  components: {
+  }
+}
+</script>
