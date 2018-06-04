d2a9ba6f55877f2792c94257701cc336e8672d48
dartchuk <manson1989>
2018-06-04 23:11:21 +0300

    start dev

---
+++ nuxt.config.js
@@ -0,0 +1,20 @@
+const router = require('./config/routes.js')
+const head = require('./config/head.js')
+
+module.exports = {
+  router,
+  head,
+  loading: { color: '#3B8070' },
+  build: {
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
