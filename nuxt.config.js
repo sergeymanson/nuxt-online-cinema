080cdca3f59cdc0875e8cbaaef9b98f289789acc
dartchuk <manson1989>
2018-06-12 23:21:37 +0300

    add element-ui

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,12 +8,22 @@ module.exports = {
   plugins: [
     {src: '~/plugins/nuxt-client-init.js', ssr: false},
     {src: '~/plugins/element.js'},
+    {src: '~/plugins/axios.js'}
   ],
   transition: 'fade',
   loading: {
     color: '#418000'
   },
-  modules: [],
+  modules: [
+    '@nuxtjs/axios',
+  ],
+  axios: {
+    debug: false,
+    withCredentials: false,
+    credentials: false,
+    baseURL: 'https://randomuser.me/api',
+    timeout: 5000
+  },
   build: {
     plugins: [
       new webpack.LoaderOptionsPlugin({ options: {} }),
